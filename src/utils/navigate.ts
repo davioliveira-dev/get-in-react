c25683c2220549c44ecd5830fdfd5e8ac97eec9c
Davi <davioliveira-dev@outlook.com>
2022-08-15 17:45:38 -0300

    refactor: add new params to navigate and refactor code

--- src/utils/navigate.ts
+++ src/utils/navigate.ts
@@ -4,6 +4,10 @@ type Params<T> = {
 };
 
 export const navigate = <T>(path: string, params?: Params<T>) => {
+	window.sessionStorage.clear();
+	if (params) {
+		window.sessionStorage.setItem(params.id, JSON.stringify(params.data));
+	}
+
 	window.location.href = `${window.location.origin}${path}`;
-	window.sessionStorage.setItem(params.id, JSON.stringify(params.data));
 };
